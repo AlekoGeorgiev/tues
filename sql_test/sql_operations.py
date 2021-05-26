75a71f070e9624d0b3db059b81f996a65ae36eaa
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2021-05-26 14:47:54 +0300

    sqlite

---
+++ sql_test/sql_operations.py
@@ -0,0 +1,49 @@
+import sqlite3
+import os.path
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "database.db")
+
+con = sqlite3.connect(db_path)
+cursor = con.cursor()
+
+name = str(input('Enter name: '))
+email = str(input('Enter email: '))
+
+cursor.execute(f"INSERT INTO user (name, email) VALUES ('{name}', '{email}')")
+con.commit()
+
+def print_all(cursor):
+    cursor.execute("SELECT * FROM user")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+print_all(cursor)
+
+def delete_row(cursor, con):
+    integer = int(input('Enter id: '))
+    sql_delete_query = f"""DELETE from user where id = {integer}"""
+    cursor.execute(sql_delete_query)
+    con.commit()
+
+delete_row(cursor, con)
+
+print_all(cursor)
+
+def update_row(cursor, con):
+    integer = int(input('Enter id: '))
+    name = str(input('Enter name: '))
+    email = str(input('Enter email: '))
+    
+    sql_update_query = f'''UPDATE user SET name='{name}', email='{email}' WHERE id = {integer} '''
+    cursor.execute(sql_update_query)
+    con.commit()
+    
+update_row(cursor, con)
+
+print_all(cursor)
+
+con.close()
